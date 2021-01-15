84ebd2ff57b70339a3014949c52f7b65a34f746c
mytsk <mn.nilsson@gmail.com>
2021-01-15 15:17:08 +0100

    fixed issues in space arrays

--- PS-StoreOnce/Functions/Report/Get-SOSIDs.psm1
+++ PS-StoreOnce/Functions/Report/Get-SOSIDs.psm1
@@ -30,31 +30,35 @@ function Get-SOSIDs {
 						} 
 					
 				$SIDsResponse = Invoke-RestMethod @SIDCall
-				$SIDCount = ($SIDsResponse.document.servicesets.serviceset).count
-				if ($SIDCount -eq $null) {$SIDCount = 1}
+				$SIDCount = ((($SIDsResponse.document.servicesets.serviceset) | Measure-Object).Count)
+				if (!$SIDCount) {$SIDCount = 1}
 				[Array] $SSID = $SIDsResponse.document.servicesets.serviceset.properties.ssid
 				[Array] $Name = $SIDsResponse.document.servicesets.serviceset.properties.name
+                [Array] $Product = $SIDsResponse.document.servicesets.serviceset.properties.productClass
 				[Array] $Alias = $SIDsResponse.document.servicesets.serviceset.properties.alias
 				[Array] $OverallHealth = $SIDsResponse.document.servicesets.serviceset.properties.overallHealth
 				[Array] $SerialNumber = $SIDsResponse.document.servicesets.serviceset.properties.serialNumber
-				[Array] $CapacityBytes = $SIDsResponse.document.servicesets.serviceset.properties.capacityBytes
-				[Array] $FreeBytes = $SIDsResponse.document.servicesets.serviceset.properties.freeBytes
-				[Array] $UserBytes = $SIDsResponse.document.servicesets.serviceset.properties.userBytes
-				[Array] $DiskBytes = $SIDsResponse.document.servicesets.serviceset.properties.diskBytes
+				[Array] $CapacityBytes = $SIDsResponse.document.servicesets.serviceset.properties.localCapacityBytes
+				[Array] $FreeBytes = $SIDsResponse.document.servicesets.serviceset.properties.localFreeBytes
+				[Array] $UserBytes = $SIDsResponse.document.servicesets.serviceset.properties.localUserBytes
+				[Array] $DiskBytes = $SIDsResponse.document.servicesets.serviceset.properties.localDiskBytes
+				[Array] $DedupeRatio = $SIDsResponse.document.servicesets.serviceset.properties.dedupeRatio
 				
 				for ($i = 0; $i -lt $SIDCount; $i++ ){		
 					$row = [PSCustomObject] @{
 						System = $($SOConnection.Server)
-						SIDCount = [String] $SIDCount
+						SIDCount = [String] $SIDCount[$i]
 						SSID = $SSID[$i]
 						Name = $Name[$i]
+						ProductClass= $Product[$i]
 						Alias = $Alias[$i]
 						OverallHealth = $OverallHealth[$i]
 						SerialNumber = $SerialNumber[$i]
-						"Capacity(GB)" = ([math]::Round(($CapacityBytes[$i] / 1073741824),2))
-						"Free(GB)" = ([math]::Round(($FreeBytes[$i] / 1073741824),2))
-						"UserData(GB)" = ([math]::Round(($UserBytes[$i] / 1073741824),2))
-						"DiskData(GB)" = ([math]::Round(($DiskBytes[$i] / 1073741824),2))
+						"Capacity(GB)" = ([math]::Round(($CapacityBytes[$i] / 1gb),2))
+						"Free(GB)" = ([math]::Round(($FreeBytes[$i] / 1gb),2))
+						"UserData(GB)" = ([math]::Round(($UserBytes[$i] / 1gb),2))
+						"DiskData(GB)" = ([math]::Round(($DiskBytes[$i] / 1gb),2))
+						"Dedeup Ratio" = $DedupeRatio[$i]
 					}
 					$SOSIDs += $row
 				} 
